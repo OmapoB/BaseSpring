dc8cac8dedfac29465cae9dc125cc9be13eff4eb
Gasan Omarov <omarov.g.r.6@gmail.com>
2024-08-29 10:06:45 +0300

    initial commit

---
+++ src/main/java/com/paygo/basespringapp/BaseSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.paygo.basespringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseSpringAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseSpringAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/paygo/basespringapp/BaseSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paygo.basespringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseSpringAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
